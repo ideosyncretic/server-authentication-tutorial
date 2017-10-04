4bef53a593c8c327c6b2fd79c1ff6ab388d5ce36
Angel Coco Chen <angel@finaxar.com>
2017-10-04 22:18:10 +0800

    App setup

---
+++ index.js
@@ -0,0 +1,18 @@
+// main starting point of the applicationnode
+
+const express = require('express')
+const http = require ('http') // low-level native Node library
+const bodyParser = require('body-parser')
+const morgan = require ('morgan')
+
+const app = express()
+
+// App setup
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*' }))
+
+// Server setup
+const port = process.env.PORT || 3090
+const server = http.createServer(app)
+server.listen(port)
+console.log('Server listening on:', port)
