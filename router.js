2a2e516ca83015de2c76bf259a7fb656da96db99
Angel Coco Chen <angel@finaxar.com>
2017-10-08 17:18:36 +0800

    Fix typo in passport.js and add requireAuth helper to router

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 const Authentication = require('./controllers/authentication')
+const passportService = require('./services/passport')
+const passport = require('passport')
+
+const requireAuth = passport.authenticate('jwt', { session: false })
+// passport defaults to trying to create a cookie-based session for authenticated users
+// we're using JWT so we turn that off
 
 module.exports = function (app) {
+  app.get('/', requireAuth, function (req, res) {
+    res.send({ hi: 'there' })
+  })
   app.post('/signup', Authentication.signup)
 }
--- services/passport.js
+++ services/passport.js
@@ -2,25 +2,29 @@ const passport = require('passport')
 const User = require('../models/user')
 const config = require('../config')
 const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').Extract
+const ExtractJwt = require('passport-jwt').ExtractJwt
 
 // Set up options for JWT Strategy
-const jwtOptions = {}
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+}
 
 // Create JWT Strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
    // See if the user ID in payload exists in our DB
-   // If it does, call 'done' with that user
-   // Otherwise, call 'done' without a user object
    User.findById(payload.sub, function(err, user) {
      if (err) { return done(err, false) } // has error
 
      if (user) {
-       done(null, user) // no error, with user
+       // If it does, call 'done' with that user
+       done(null, user)
      } else {
-       done(null, false) // no error, without user
+       // Otherwise, call 'done' without a user object
+       done(null, false)
      }
    })
 })
 
 // Tell Passport to use this strategy
+passport.use(jwtLogin)
