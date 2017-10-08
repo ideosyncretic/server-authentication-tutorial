c64df3c5f77e10898c76e5424ba604271440e61a
Angel Coco Chen <angel@finaxar.com>
2017-10-08 14:27:21 +0800

    Require both email and password on signup

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,6 +4,10 @@ exports.signup = function (req, res, next) {
   const email = req.body.email
   const password = req.body.password
 
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Please provide both email and password' })
+  }
+
   // see if a user with the given email already exists
   User.findOne({ email: email }, function(err, existingUser) {
     // second arg to findOne is callback. everything with Node happens with callbacks
@@ -26,6 +30,6 @@ exports.signup = function (req, res, next) {
     }) // saves user to the DB
 
     // respond to request indicating the user was created
-    res.json({ success: 'true' })
+    res.json({ success: true })
   })
 }
