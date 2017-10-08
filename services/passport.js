12ba1e6b16d0d80a8ee4b352485a3f981465c6fc
Angel Coco Chen <angel@finaxar.com>
2017-10-08 17:32:37 +0800

    Create local strategy for sign in requests

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,21 @@ const User = require('../models/user')
 const config = require('../config')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+const LocalStrategy = require('passport-local')
+
+// Create local strategy
+const localOptions = { usernameField: 'email' } // point to email property, since we use email instead of username
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+ // Verify this email and password, then call done with user
+ // If this is correct email and password
+ // If not, call done with false
+ User.findOne({ email: email }, function (err, user) {
+   if (err) { return done(err) }
+   if (!user) { return done(null, false) }
+ })
+
+ // compare plain password input with stored hashed password
+})
 
 // Set up options for JWT Strategy
 const jwtOptions = {
