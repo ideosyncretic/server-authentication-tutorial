b7ca913a224cd00992b26794adf81c9cd56f41c5
Angel Coco Chen <angel@finaxar.com>
2017-10-08 14:23:18 +0800

    Add authentication logic for creating new user account

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,13 +6,26 @@ exports.signup = function (req, res, next) {
 
   // see if a user with the given email already exists
   User.findOne({ email: email }, function(err, existingUser) {
+    // second arg to findOne is callback. everything with Node happens with callbacks
+    // existingUser param will be null if findOne does not find a user with matching email
+    if (err) { return next(err) }
 
-  }) // second arg to findOne is callback. everything with Node happens with callbacks
-  // existingUser param will be null if findOne does not find a user with matching email
+    // if a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' })
+    }
 
-  // if a user with email does exist, return an error
+    // if no existing user with email, create and save record
+    const user = new User({
+      email: email,
+      password: password
+    }) // this only creates a User instance in memory
 
-  // if no existing user with email, create and save record
+    user.save(function (err) {
+      if (err) { return next(err) }
+    }) // saves user to the DB
 
-  // respond to request indicating the user was created
+    // respond to request indicating the user was created
+    res.json({ success: 'true' })
+  })
 }
